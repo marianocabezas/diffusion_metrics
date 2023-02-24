4cab6da834c0bc929d681d8e5d67147845d67aad
mcabezas <mcabezas@eia.udg.edu>
2023-02-24 11:55:13 +1100

    fixed angular correlation, added vector selection for nodes

--- fod.py
+++ fod.py
@@ -7,7 +7,7 @@ def mean_squared_error(target, source, roi=None):
         source = source[roi.astype(bool)]
         target = target[roi.astype(bool)]
 
-    return np.sum((target - source) ** 2, axis=1)
+    return np.sum((target - source) ** 2, axis=-1)
 
 
 def mean_absolute_error(target, source, roi=None):
@@ -15,7 +15,7 @@ def mean_absolute_error(target, source, roi=None):
         source = source[roi.astype(bool)]
         target = target[roi.astype(bool)]
 
-    return np.sum(np.abs(target - source), axis=1)
+    return np.sum(np.abs(target - source), axis=-1)
 
 
 def psnr(target, source, roi=None, pixel_max=1):
